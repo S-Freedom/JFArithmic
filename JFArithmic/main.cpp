6aeba5be27ea1610949884f054679de085faca2c
huangpengfei <huangpengfei@100tal.com>
2022-01-18 18:36:45 +0800

    Initial Commit

---
+++ JFArithmic/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  JFArithmic
+//
+//  Created by 黄鹏飞 on 2022/1/18.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
